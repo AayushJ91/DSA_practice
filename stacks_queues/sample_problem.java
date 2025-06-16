f55d6fcb5ae6b17746f9f1dff257fd1029a05204
Aayush <aayushjha2431@gmail.com>
2025-06-16 10:50:57 +0530

    adding all the files

---
+++ stacks_queues/sample_problem.java
@@ -0,0 +1,12 @@
+// Problem: Sample Problem
+// Link: https://example.com/problem
+// Source: LeetCode
+// Topics: Arrays
+// Difficulty: Easy
+
+class Solution {
+    public int[] sampleFunction(int[] nums) {
+        // Your logic here
+        return new int[]{};
+    }
+}
