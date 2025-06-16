eee5fbc690a2674612e185528f81cb5f2fa82b45
Aayush <aayushjha2431@gmail.com>
2025-06-16 11:06:29 +0530

    Please enter the commit message for your changes. Lines starting cleared the two sum

--- arrays/TwoSum.java
+++ arrays/TwoSum.java
@@ -7,16 +7,6 @@
 import java.util.*;
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> pairIdx = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            if (pairIdx.containsKey(target - num)) {
-                return new int[] { i, pairIdx.get(target - num) };
-            }
-            pairIdx.put(num, i);
-        }
-
-        return new int[] {};
     }
 }
