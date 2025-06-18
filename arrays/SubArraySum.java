c11ef039682938bcec621f9111905ea11749c02b
Aayush <aayushjha2431@gmail.com>
2025-06-18 10:50:51 +0530

    solved maxSubArrays

--- arrays/SubArraySum.java
+++ arrays/SubArraySum.java
@@ -6,39 +6,35 @@
 //Time:
 //space:
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 class SubArraySum {
     static int subarraySum(int[] nums, int k) {
-        int n = nums.length;
-        int res = 0;
-        HashMap<Integer, Integer> idx = new HashMap<>();
-        int sum = 0;
+        int n = nums.length, res = 0;
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            idx.put(i, sum);
-        }
-        int total = idx.get(n - 1);
-        if (total == k) {
-            res++;
-        }
-        int diff, end = n - 1, fro = 0;
-        while (fro < n && end >= 0){
-            diff = idx.get(end) - idx.get(fro);
-            if (diff == k) {
+            if (prefix[i] == k) {
+                //agar i tak sum is equal to k
                 res++;
-                fro++;
-            } else if (diff > k) {
-                fro++;
-            }else {
-                end--;
             }
+            if (map.containsKey(prefix[i] - k)) {
+                //checking the subarrays
+                res += map.get(prefix[i] - k);
+            }
+            map.put(prefix[i], map.getOrDefault(prefix[i], 0) + 1);
+            //adding sum and freq to the hashmap
         }
         return res;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,-1,1};
-        System.out.println(subarraySum(nums, 3));
+        int[] nums = {1,1,2,5};
+        System.out.println(subarraySum(nums, 7));
     }
 }
